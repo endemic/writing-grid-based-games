1dd35793fefb46951f8167fccaac37c7a454aa64
Nathan Demick <n@demick.org>
2024-11-22 10:53:29 -0500

    Update processor.rb

--- processor.rb
+++ processor.rb
@@ -4,25 +4,18 @@ require 'yaml'
 require 'kramdown'
 
 # load HTML template
-# file needs to have `%{body}`, %{description}, etc. to be interpolated
 html_template = File.read('template.html')
 
 Dir.glob('*.yml').each do |file|
   puts "Processing #{file}"
   template_data = YAML.load_file(file)
 
-  # what fields do we want in the src?
-  # title
-  # author
-  # description
-  # body
-
   # mutate body to HTML
-  template_data['body'] = Kramdown::Document.new(template_data['body']).to_html
+  template_data['content'] = Kramdown::Document.new(template_data['content']).to_html
 
   # interpolate body into template
   html = html_template % template_data.transform_keys(&:to_sym)
 
   # write output
-  File.write(Dir.join('dist', file.gsub(/\.yml$/, '.html')) , html)
+  File.write("dist/#{file.gsub(/\.yml$/, '.html')}", html)
 end
